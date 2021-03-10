453094ae3bd0a9e081492d8bb46962961443e474
kaitohkid <75655973+kaitohkid@users.noreply.github.com>
2021-03-10 11:51:33 +0700

    homework day 8-3

---
+++ homework/8-3-2021/EX 1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+   <head>
+        <title>  excercise 1 </title>
+   </head>
+<body>
+<?php
+  $a= x ;
+  $b= y ;
+  if (x=y)
+  {
+      echo" tổng=:".(x+y);
+  }
+      else
+      {
+          echo"tổng=:",( (x+y)*3);
+      }
+?>
+</body>
+</html>
